194d39d3c000a276196323fe01957153539456e0
Samir1716 <123534782+Samir1716@users.noreply.github.com>
2023-03-17 19:54:50 +0530

    Module 3-3

---
+++ C- Programming/Pattern_1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int rows = 4;
+    
+    for(int i=0; i<=rows; i++) {
+        for(int j=0; j<=i; j++) {
+            if(j%2 == 0) {
+                printf("1 ");
+            }
+            else {
+                printf("0 ");
+            }
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
