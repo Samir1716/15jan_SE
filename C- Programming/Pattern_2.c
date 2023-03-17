5f9f4e951384bdf5e2806db5d62bb59d34a0e17b
Samir1716 <123534782+Samir1716@users.noreply.github.com>
2023-03-17 19:56:53 +0530

    Module 3-3

---
+++ C- Programming/Pattern_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int rows = 5;
+    int count = 65;
+    
+    for(int i=0; i<rows; i++) {
+        for(int j=0; j<=i; j++) {
+            printf("%c ", count);
+            count++;
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
