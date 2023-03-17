f2d9e6b7ff5416b2d9ccdc3ba572e3fe71593584
Samir1716 <123534782+Samir1716@users.noreply.github.com>
2023-03-17 20:07:34 +0530

    Module 3-3

---
+++ C- Programming/Pattern_4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int rows = 6;
+    int i, j;
+
+    // print upper half of pattern
+    for(i=1; i<=rows; i++) {
+        for(j=1; j<=i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    // print lower half of pattern
+    for(i=rows-1; i>=1; i--) {
+        for(j=1; j<=i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
