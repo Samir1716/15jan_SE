e0c52e0c880b1504891ef1e18eb7f9b103bce8ba
Samir1716 <123534782+Samir1716@users.noreply.github.com>
2023-03-17 20:19:20 +0530

    Module 3-2

--- C- Programming/Pattern_2.c
+++ C- Programming/Pattern_2.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
     int rows = 5;
     int count = 65;
     
-    for(int i=0; i<rows; i++) {
-        for(int j=0; j<=i; j++) {
+    for(int i=0; i<rows; i++) 
+    {
+        for(int j=0; j<=i; j++)
+        {
             printf("%c ", count);
             count++;
         }
