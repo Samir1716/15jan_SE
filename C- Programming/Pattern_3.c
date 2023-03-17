0e322980f3c16728a39624a51f8c5622dce595f8
Samir1716 <123534782+Samir1716@users.noreply.github.com>
2023-03-17 20:20:14 +0530

    Module 3-2

--- C- Programming/Pattern_3.c
+++ C- Programming/Pattern_3.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
     int rows = 5;
     int i, j, space;
 
-    for(i=1; i<=rows; i++) {
+    for(i=1; i<=rows; i++) 
+    {
         // print spaces before each row
-        for(space=1; space<=rows-i; space++) {
+        for(space=1; space<=rows-i; space++) 
+        {
             printf(" ");
         }
 
         // print asterisks for current row
-        for(j=1; j<=2*i-1; j++) {
+        for(j=1; j<=2*i-1; j++) 
+        {
             printf("*");
         }
 
