569e033627c7d0c404fb34ad3f8b3540d32df205
Samir1716 <123534782+Samir1716@users.noreply.github.com>
2023-03-28 19:35:29 +0530

    Module 4-2

---
+++ C++ Programing/Calculator_using_class.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+class Calculator
+{
+    public:
+        float num1, num2;
+        char op;
+
+        void calculate() 
+        {
+            switch(op) 
+            {
+                case '+':
+                    cout << "Result: " << num1 + num2;
+                    break;
+                case '-':
+                    cout << "Result: " << num1 - num2;
+                    break;
+                case '*':
+                    cout << "Result: " << num1 * num2;
+                    break;
+                case '/':
+                    if(num2 == 0) 
+                    {
+                        cout << "Error: Division by Zero";
+                    } 
+                else 
+                    {
+                        cout << "Result: " << num1 / num2;
+                    }
+                    break;
+                default:
+                    cout << "Invalid Operator..!!";
+                    break;
+            }
+        }
+};
+
+int main() 
+{
+    Calculator c;
+    cout << "Enter First Number: ";
+    cin >> c.num1;
+    cout << "Enter Operator (+,-,*,/): ";
+    cin >> c.op;
+    cout << "Enter Second Number: ";
+    cin >> c.num2;
+    c.calculate();
+    return 0;
+}
