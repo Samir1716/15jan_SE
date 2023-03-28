1eba18a99b55dd453e2f503ac595f20726d974b1
Samir1716 <123534782+Samir1716@users.noreply.github.com>
2023-03-28 19:53:26 +0530

    Module 4-2

---
+++ C++ Programing/Maltiplication_and_cubic_value_using_inline_function.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+inline int multiply(int num1, int num2) 
+{
+    return num1 * num2;
+}
+
+inline int cube(int num)
+{
+    return num * num * num;
+}
+
+int main() 
+{
+    int num1, num2;
+    cout << "Enter Two Numbers: ";
+    cin >> num1 >> num2;
+
+    int product = multiply(num1, num2);
+    cout << "Multiplication: " << product << endl;
+
+    int num3;
+    cout << "Enter a Number to Find its Cube : ";
+    cin >> num3;
+
+    int cubeValue = cube(num3);
+    cout << "Cubic value: " << cubeValue << endl;
+
+    return 0;
+}
