76c37755e3407cb845b31bce09ebc54f18e726d9
Samir1716 <123534782+Samir1716@users.noreply.github.com>
2023-03-29 19:55:30 +0530

    Module 4-2

---
+++ C++ Programing/Concatenate_the_two_strings_using_Operator_overloading.cpp
@@ -0,0 +1,49 @@
+#include <iostream.h>
+#include <cstring.h>
+using namespace std;
+
+class String 
+{
+    private:
+        char *str;
+
+    public:
+        String() 
+        {
+            str = NULL;
+        }
+
+        String(const char *s)
+        {
+            str = new char[strlen(s) + 1];
+            strcpy(str, s);
+        }
+
+        ~String() 
+        {
+            if (str) delete [] str;
+        }
+
+        void display()
+        {
+            cout << str << endl;
+        }
+
+        String operator+(String& s)
+        {
+            char *temp = new char[strlen(str) + strlen(s.str) + 1];
+            strcpy(temp, str);
+            strcat(temp, s.str);
+            String result(temp);
+            delete [] temp;
+            return result;
+        }
+};
+
+int main() 
+{
+    String s1("Hello"), s2(" World!");
+    String s3 = s1 + s2;
+    s3.display();
+    return 0;
+}
